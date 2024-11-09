51b239ffe732a096e76b0f9f32285cc84d17280a
Felipe <chavezfelipe189@gmail.com>
2024-11-09 12:56:11 -0300

    hay base de datos x2

--- src/app/pages/pc/pc.page.ts
+++ src/app/pages/pc/pc.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ServiceBDService } from 'src/app/services/service-bd.service';
 
 @Component({
   selector: 'app-pc',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PcPage implements OnInit {
 
-  constructor() { }
+  productosP: any[] = []; // Arreglo para almacenar los productos
 
-  ngOnInit() {
+  constructor(private alertController: AlertController, private serviceBD: ServiceBDService, private router: Router) { }
+
+  
+  async ionViewWillEnter() {
+    this.cargarProductos(); // Cargar productos al inicializar
   }
+  async ngOnInit() {
+    this.cargarProductos(); // Cargar productos al inicializar
+  }
+
+    // Función para obtener todos los productos
+    cargarProductos() {
+      this.serviceBD.seleccionarPC().then((productos) => {
+        this.productosP = productos;
+      }).catch((error) => {
+        console.error('Error al cargar los productos:', error);
+      });
+    }
+
+    irproductoSolo(x: any) {
+      let navigationExtras: NavigationExtras = {
+        state: {
+          productoVa: x
+        }
+      };
+      this.router.navigate(['/detalle-producto'], navigationExtras);
+    }
 
 }
--- src/app/pages/sillas/sillas.page.ts
+++ src/app/pages/sillas/sillas.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ServiceBDService } from 'src/app/services/service-bd.service';
 
 @Component({
   selector: 'app-sillas',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SillasPage implements OnInit {
 
-  constructor() { }
+  productosS: any[] = []; // Arreglo para almacenar los productos
 
-  ngOnInit() {
+  constructor(private alertController: AlertController, private serviceBD: ServiceBDService, private router: Router) { }
+
+  
+  async ionViewWillEnter() {
+    this.cargarProductos(); // Cargar productos al inicializar
   }
+  async ngOnInit() {
+    this.cargarProductos(); // Cargar productos al inicializar
+  }
+
+    // Función para obtener todos los productos
+    cargarProductos() {
+      this.serviceBD.seleccionarSillas().then((productos) => {
+        this.productosS = productos;
+      }).catch((error) => {
+        console.error('Error al cargar los productos:', error);
+      });
+    }
+
+    irproductoSolo(x: any) {
+      let navigationExtras: NavigationExtras = {
+        state: {
+          productoVa: x
+        }
+      };
+      this.router.navigate(['/detalle-producto'], navigationExtras);
+    }
 
 }
