64c82f558c1018e3bd03154d0af19eca57ea55f7
Alonso <amuc23@gmail.com>
2024-11-12 22:20:33 -0300

    arreglo de retornar

--- src/app/pages/crud/crud.page.ts
+++ src/app/pages/crud/crud.page.ts
@@ -16,9 +16,7 @@ export class CrudPage implements OnInit {
   constructor(private alertController: AlertController, private serviceBD: ServiceBDService, private router: Router) { }
 
   
-  async ionViewWillEnter() {
-    this.cargarProductos(); // Cargar productos al inicializar
-  }
+
   async ngOnInit() {
     this.cargarProductos(); // Cargar productos al inicializar
   }
